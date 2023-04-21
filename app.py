cfc005073162924869d8e1827f0148310c366e7d
Sean Jesiolowski <sean.jesiolowski@gmail.com>
2023-04-20 21:53:03 -0400

    Add more cities, more temp types

--- app.py
+++ app.py
@@ -6,21 +6,58 @@ class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title(f"Current Temperature (F) in {w.city}")
-        self.minsize(400, 300)
+        # self.title("The Weather in Three Cities")
 
-        self.btn1 = ctk.CTkButton(
-            master=self, command=self.button_callback, text='Get the weather')
-        self.btn1.pack(padx=20, pady=20)
+        self.main_frame = ctk.CTkFrame(self, width=1020, height=951)
+        self.main_frame.grid_columnconfigure(0, minsize=600)
+        self.main_frame.grid_columnconfigure(1, minsize=600)
+        self.main_frame.grid()
 
-        self.txtbx1 = ctk.CTkTextbox(master=self)
-        self.txtbx1.pack(padx=20, pady=20)
+        self.drop_down = ctk.CTkOptionMenu(
+            self.main_frame, width=300, values=['Lowville', 'Watertown', 'Rochester'], font=('Helvetica bold', 30), command=self.send_value)
+        self.drop_down.grid(row=0, column=0, padx=0, pady=0)
+        self.city = 'Lowville'
+
+        self.btn1 = ctk.CTkButton(self.main_frame, width=300, command=self.button_callback,
+                                  text='Get temps', font=('Helvetica bold', 30))
+        self.btn1.grid(row=0, column=1, padx=0, pady=0)
+
+        self.data_frame = ctk.CTkFrame(self.main_frame)
+        self.data_frame.grid_columnconfigure(0, minsize=350)
+        self.data_frame.grid_columnconfigure(1, minsize=350)
+        self.data_frame.grid_columnconfigure(2, minsize=350)
+        self.data_frame.grid(columnspan=2, padx=20)
+
+        #!  0
+        self.lbl0 = ctk.CTkLabel(self.data_frame, text='low', font=(
+            'Helvetica bold', 30))
+        self.lbl0.grid(row=0, column=0, padx=0, pady=200)
+
+        #!  1
+        self.lbl1 = ctk.CTkLabel(self.data_frame, text='current', font=(
+            'Helvetica bold', 50))
+        self.lbl1.grid(row=0, column=1, padx=0, pady=200)
+
+        #!  2
+        self.lbl2 = ctk.CTkLabel(self.data_frame, text='high', font=(
+            'Helvetica bold', 30))
+        self.lbl2.grid(row=0, column=2, padx=0, pady=200)
+
+        print(self.main_frame.winfo_width())
+        print(self.main_frame.winfo_height())
 
     def button_callback(self):
-        current_weather = w.Weather()
-        self.txtbx1.delete("1.0", ctk.END)  # "line.column"
-        message = f"At {current_weather.current_time},\nthe temperature in {current_weather.city}\nis {current_weather.get_temp_f()} degrees Fahrenheit."
-        self.txtbx1.insert("insert", message, "n/a")
+        current_weather = w.Weather(self.city)
+
+        self.lbl0.configure(text=f'{current_weather.get_list_f()[0]}F')
+        self.lbl1.configure(text=f'{current_weather.get_list_f()[1]}F')
+        self.lbl2.configure(text=f'{current_weather.get_list_f()[2]}F')
+
+        print(self.winfo_width())
+        print(self.winfo_height())
+
+    def send_value(self, city):
+        self.city = city
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -5,8 +5,13 @@ import json
 city = 'Lowville'
 
 
+def select_city(app_city):
+    global city
+    city = app_city
+
+
 class Weather:
-    def __init__(self):
+    def __init__(self, city):
         self.city = city
         self.current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
@@ -15,20 +20,15 @@ class Weather:
         response_api = requests.get(api_url)
         print("API response status code:", response_api.status_code)
         raw_data = response_api.text
-        parse_json = json.loads(raw_data)
-        filepath = 'weather.json'
-        with open(filepath, 'w') as f:
-            json.dump(parse_json, f)
-        print("Data saved as JSON")
 
-    def print_json(self):
-        with open("weather.json", "r") as f:
-            data = json.load(f)
-        json_string = json.dumps(data, indent=4)
-        print(f'{json_string}\n')
+        self.parse_json = json.loads(raw_data)
 
-    def get_temp_f(self):
-        with open("weather.json", "r") as f:
-            temp_k = json.load(f)["main"]["temp"]
-        temp_f = (temp_k - 273.15) * 9/5 + 32
-        return format(temp_f, '.4f')
+    def get_list_f(self):
+        # with open("weather.json", "r") as f:
+        min_k = self.parse_json["main"]["temp_min"]
+        curr_k = self.parse_json["main"]["temp"]
+        max_k = self.parse_json["main"]["temp_max"]
+        k_list = [min_k, curr_k, max_k]
+        f_list = [format(((item - 273.15) * 9/5 + 32), '.2F')
+                  for item in k_list]
+        return f_list
